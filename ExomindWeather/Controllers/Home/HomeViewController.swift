cd362918198c9a532ab80c97f3fd31d9ffb08f7c
Reqven <manuu.marchand@gmail.com>
2022-04-11 14:34:20 +0200

    Add a button to navigate to the Weather ViewController

--- ExomindWeather/Controllers/Home/HomeViewController.swift
+++ ExomindWeather/Controllers/Home/HomeViewController.swift
@@ -8,11 +8,42 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+  
+  //MARK: - Properties
+  private let button = UIButton()
 
+  //MARK: - Methods
   override func viewDidLoad() {
     super.viewDidLoad()
+    setup()
+    setupLayout()
+  }
+  
+  @objc private func onTouch(_ sender: UIButton?) {
+    let controller = WeatherViewController()
+    navigationController?.pushViewController(controller, animated: true)
+  }
+}
+
+
+// MARK: - Setup
+extension HomeViewController {
+  
+  private func setup() {
+    title = "Home"
+    view.backgroundColor = .systemBackground
+    button.setTitle("Weather", for: .normal)
+    button.setTitleColor(view.tintColor, for: .normal)
+    button.addTarget(self, action: #selector(onTouch), for: .touchUpInside)
+  }
+  
+  private func setupLayout() {
+    button.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(button)
     
-    self.title = "Home"
-    self.view.backgroundColor = .systemBackground
+    NSLayoutConstraint.activate([
+      button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+    ])
   }
 }
