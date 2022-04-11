9c05edf2e59bc175a7f7d8617f5327d79830fc51
Reqven <manuu.marchand@gmail.com>
2022-04-11 14:17:53 +0200

    Add a NavigationController with Home and Weather ViewControllers

---
+++ ExomindWeather/Controllers/Weather/WeatherViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherViewController.swift
+//  ExomindWeather
+//
+//  Created by Manu on 11/04/2022.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    self.title = "Weather"
+    self.view.backgroundColor = .systemBackground
+  }
+}
--- ExomindWeather/SceneDelegate.swift
+++ ExomindWeather/SceneDelegate.swift
@@ -18,11 +18,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
     guard let windowScene = (scene as? UIWindowScene) else { return }
     
+    let navigationController = UINavigationController(rootViewController: HomeViewController())
+    navigationController.navigationBar.prefersLargeTitles = true
+    
     window = UIWindow(frame: windowScene.coordinateSpace.bounds)
     window?.makeKeyAndVisible()
     window?.tintColor = .systemBlue
     window?.windowScene = windowScene
-    window?.rootViewController = ViewController()
+    window?.rootViewController = navigationController
   }
 
   func sceneDidDisconnect(_ scene: UIScene) {
